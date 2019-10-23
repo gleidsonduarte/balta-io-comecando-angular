93a262eb7b3af867883fc7cf79b5f0dd1aeb502f
Gleidson Teixeira Duarte <gleidson.duarte@pravaler011.idealinvest.local>
2019-10-23 19:58:07 -0300

    Continuação do curso.]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,12 +21,15 @@ export class AppComponent {
         Validators.required
       ])]
     });
+
+    this.load();
   }
 
   add() {
     const title = this.form.controls['title'].value;
     const id = this.todos.length + 1;
     this.todos.push(new Todo(id, title, false));
+    this.save();
     this.clear();
   }
 
@@ -40,17 +43,29 @@ export class AppComponent {
 
   remove(todo: Todo) {
     const index = this.todos.indexOf(todo);
-
     if (index !== -1) {
       this.todos.splice(index, 1);
     }
+    this.save();
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
+    this.save();
   }
 
   markAsUndone(todo: Todo) {
     todo.done = false;
+    this.save();
+  }
+
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos')
+    this.todos = JSON.parse(data)
   }
 }
